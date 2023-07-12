6d7758ab4f748c1a43bb35f5ce81241c185cee8c
John Cortell <j.cortell@sarc.samsung.com>
2023-07-12 16:01:57 -0500

    Simple view contribution with a Tree View

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,21 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Simple tree data provider for our view. Just returns three items.
+export class MyTreeDataProvider implements vscode.TreeDataProvider<String> {
+	getTreeItem(element: String): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return new vscode.TreeItem("" + element);
+	}
+	getChildren(element?: String | undefined): vscode.ProviderResult<String[]> {
+		return Promise.resolve(["this", "that", "whatever"]);
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	vscode.window.registerTreeDataProvider("myext.view", new MyTreeDataProvider());
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -19,15 +31,8 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from myext!');
 	});
 	context.subscriptions.push(disposable);
-
-	disposable = vscode.commands.registerCommand('myext.goodbyeWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Goodbye World from myext!');
-	});
-	context.subscriptions.push(disposable);
-
 }
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
