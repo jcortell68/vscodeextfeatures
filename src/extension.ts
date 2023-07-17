27bae9bd7bb765e95aab6202014ce4b85855a0ec
John Cortell <j.cortell@sarc.samsung.com>
2023-07-17 09:17:28 -0500

    Adds a viewsWelcome contribution

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,8 @@ export class MyTreeDataProvider implements vscode.TreeDataProvider<String> {
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.window.registerTreeDataProvider("myext.view", new MyTreeDataProvider());
+	// Disabling this so our viewsWelcome is shown...
+	// vscode.window.registerTreeDataProvider("myext.view", new MyTreeDataProvider());
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
