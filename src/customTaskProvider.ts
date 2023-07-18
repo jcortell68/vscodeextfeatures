8d7c07d92393eef7b0caa340eda6574aedcb0917
John Cortell <j.cortell@sarc.samsung.com>
2023-07-18 08:17:11 -0500

    Fix bug; task didn't complete.

--- src/customTaskProvider.ts
+++ src/customTaskProvider.ts
@@ -3,10 +3,24 @@ import * as vscode from 'vscode';
 class CustomTaskTerminal implements vscode.Pseudoterminal {
 	private writeEmitter = new vscode.EventEmitter<string>();
 	onDidWrite: vscode.Event<string> = this.writeEmitter.event;
+	private closeEmitter = new vscode.EventEmitter<number>();
+	onDidClose?: vscode.Event<number> = this.closeEmitter.event;
+
 	open(initialDimensions: vscode.TerminalDimensions | undefined): void {
-		this.writeEmitter.fire("This is some example output\n\r");
+		const output = 'somefile.abc:1:4: warning: the roof is on fire ';
+		this.doBuild(output);
 	}
 	close(): void {
+		this.writeEmitter.dispose();
+		this.closeEmitter.dispose();
+	}
+	private async doBuild(output: string): Promise<void> {
+		return new Promise<void>((resolve) => {
+			this.writeEmitter.fire('Starting build...\r\n');
+			this.writeEmitter.fire(output + '\r\n');
+			this.closeEmitter.fire(0);
+			resolve();
+		});
 	}
 }
 
