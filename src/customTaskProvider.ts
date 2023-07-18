fad303c51aaf490887d92676a6a272c2a5eb18c0
John Cortell <j.cortell@sarc.samsung.com>
2023-07-18 08:58:49 -0500

    Switched to vscode.ShellExecution
    Instead of running code in the VS Code extension, invoking the
    task now just invokes a cmdline in a shell session.
    
    For this to work, create a test.cmd that echos something, and put
    the cmd file in a directory that's in the PATH environment variable.

--- src/customTaskProvider.ts
+++ src/customTaskProvider.ts
@@ -1,29 +1,5 @@
 import * as vscode from 'vscode';
 
-class CustomTaskTerminal implements vscode.Pseudoterminal {
-	private writeEmitter = new vscode.EventEmitter<string>();
-	onDidWrite: vscode.Event<string> = this.writeEmitter.event;
-	private closeEmitter = new vscode.EventEmitter<number>();
-	onDidClose?: vscode.Event<number> = this.closeEmitter.event;
-
-	open(initialDimensions: vscode.TerminalDimensions | undefined): void {
-		const output = 'somefile.abc:1:4: warning: the roof is on fire ';
-		this.doBuild(output);
-	}
-	close(): void {
-		this.writeEmitter.dispose();
-		this.closeEmitter.dispose();
-	}
-	private async doBuild(output: string): Promise<void> {
-		return new Promise<void>((resolve) => {
-			this.writeEmitter.fire('Starting build...\r\n');
-			this.writeEmitter.fire(output + '\r\n');
-			this.closeEmitter.fire(0);
-			resolve();
-		});
-	}
-}
-
 export class CustomTaskProvider implements vscode.TaskProvider {
 	static type = 'mytasktype';
 
@@ -44,8 +20,6 @@ export class CustomTaskProvider implements vscode.TaskProvider {
 			vscode.TaskScope.Workspace,
 			`my source`,
 			CustomTaskProvider.type,
-			new vscode.CustomExecution(async (): Promise<vscode.Pseudoterminal> => {
-				return new CustomTaskTerminal();
-			}));
+			new vscode.ShellExecution("test.cmd"));
 	}
 }
