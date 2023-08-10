91c0d0be336d813eb80a8a259c3c428a6c5ef85e
John Cortell <j.cortell@sarc.samsung.com>
2023-08-10 13:47:17 -0500

    Make use of a WebviewPanelSerializer
    The serializer is used to restore the webview if VS Code is closed
    while the webview is open. The restoration happens when VS Code
    is next launched.

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,53 @@ import * as vscode from 'vscode';
 import { readFileSync, createWriteStream } from 'fs';
 import { get } from 'http';
 
+let serializedCount : undefined | number;
+
+// A webview panel serializer comes into play only when VS Code is opened
+// after it was closed while a webview panel was open (i.e., not disposed).
+// I.e., if at the moment VS Code is closed, there is a webview open, then
+// any state the webview has stored away via vscode.setState() ends up
+// getting persisted to disk. Upon relaunching VS Code, VS Code tries to
+// automatically recreate and open the webview...but it doesn't know how
+// to recreate the content. That logic is within the VS Code extension that
+// contributes the webview. That's where a WebviewPanelSerializer comes in.
+// VS Code creates the webview panel object, then calls the serializer
+// giving it any saved state the webview had (again, from setState()). The
+// The serializer is responsible for creating the webview's HTML content
+// from that state.
+//
+// Note that the deserializer only comes into play if the webview is open
+// when VS Code closes. If you explicitly clos the webview (i.e., dispose
+// it) then close VS Code, this serialization mechanism does not come into
+// play at all.
+//
+// Also note that the VS Code extension must register for an activationEvent
+// such that the extension is activated if a webview panel needs to be restored
+// on launch of VS Code. See
+// https://code.visualstudio.com/api/references/activation-events#onWebviewPanel
+class MyWebviewSerializer implements vscode.WebviewPanelSerializer {
+
+	constructor(private context: vscode.ExtensionContext) {
+	}
+
+	async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+		// `state` is the state persisted using `setState` inside the webview
+		console.log(`MyWebviewSerializer.deserializeWebviewPanel() called. state = ${state}`);
+		console.log(`state.count = ${state.count}`);
+		if (state.count) {
+			serializedCount = state.count;
+		}
+
+		// This is copied from the code that creates the webview initially (see below)
+		const imgLocalUri = vscode.Uri.joinPath(this.context.extensionUri, 'resources', 'bnb.png');
+		const imgWebviewUri = webviewPanel.webview.asWebviewUri(imgLocalUri);
+		const workerLocalUri = vscode.Uri.joinPath(this.context.extensionUri, 'src', 'webworker.js');
+		const workerWebviewUri = webviewPanel.webview.asWebviewUri(workerLocalUri);
+
+		webviewPanel.webview.html = getWebviewContent("Hi!", imgWebviewUri, workerWebviewUri, serializedCount);
+	}
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -51,7 +98,7 @@ export function activate(context: vscode.ExtensionContext) {
 		workerWebviewUri = panel.webview.asWebviewUri(workerLocalUri);
 
 		// Show some initial conent
-		panel.webview.html = getWebviewContent("Hi!", imgWebviewUri, workerWebviewUri);
+		panel.webview.html = getWebviewContent("Hi!", imgWebviewUri, workerWebviewUri, serializedCount);
 
 		// Stop updating the  webview content if the panel is closed. Otherwise
 		// the code will experience an exception every second until the app
@@ -65,13 +112,22 @@ export function activate(context: vscode.ExtensionContext) {
 		);
 
 		panel.webview.onDidReceiveMessage(message => {
+			console.log("extension received message from webview panel");
 			switch (message.command) {
 				case 'alert':
 					vscode.window.showInformationMessage('Rounds = ' + message.roundVal);
+					break;
+				case 'saveCount':
+					vscode.window.showInformationMessage('Asked to save count');
+					break;
+
 			}
 		});
 
 	});
+
+	// And make sure we register a serializer for our webview type
+	vscode.window.registerWebviewPanelSerializer('myextWebviewPanelType', new MyWebviewSerializer(context));
 
 	context.subscriptions.push(disposable);
 
@@ -103,7 +159,10 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-function getWebviewContent(msg: string, img: vscode.Uri, workerUri: vscode.Uri) {
+function getWebviewContent(msg: string, img: vscode.Uri, workerUri: vscode.Uri, serializedCount: number | undefined) {
+	if (!serializedCount) {
+		serializedCount = 0;
+	}
 	return `
 	<!DOCTYPE html>
 	<html lang="en">
@@ -147,7 +206,15 @@ function getWebviewContent(msg: string, img: vscode.Uri, workerUri: vscode.Uri) 
 
 			const prevState = vscode.getState();
 			console.log("prevState = " + prevState);
-			let count = prevState ? prevState.count : 0;
+			let count = 0;
+			if (prevState) {
+				count = prevState.count;
+				console.log("setting count from prevState to " + prevState);
+			}
+			else {
+				count = ${serializedCount};
+				console.log("setting count to " + ${serializedCount});
+			}
 
 			const counter = document.getElementById('lines-of-code-counter');
 			let rounds = ~~(count/10); // A round is a sequence of 10
@@ -170,7 +237,6 @@ function getWebviewContent(msg: string, img: vscode.Uri, workerUri: vscode.Uri) 
 						break;
 				}
 			});
-
     	</script>
 	  </div>
 	</body>
