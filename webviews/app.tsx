214e33503de1daa261662f02eb16af2fd00d71b6
John Cortell <j.cortell@samsung.com>
2023-10-31 13:48:39 -0500

    wip

--- webviews/app.tsx
+++ webviews/app.tsx
@@ -1,21 +1,28 @@
 import { render } from 'react-dom';
-import React, { useState } from 'react';
+import React, { KeyboardEvent, useState, useEffect, useRef } from 'react';
 
 export function main() {
-    render(<MyList/>, document.getElementById('app'));
+    render(<MyComponent/>, document.getElementById('app'));
 }
 
+async function runQuery(query: string) {
 
-export default function MyList() {
-    function handleClick() {
-      setIsShown(current => !current);
+}
+
+export default function MyComponent() {
+    const myref = useRef<HTMLTextAreaElement>(null);
+    function keyDown(event: KeyboardEvent) {
+      if (event.ctrlKey && event.key === 'Enter') {
+        console.log(`jjjjjjjjjjjjjjjjjj BOOM`);
+        console.log(myref.current?.value);
+      }
     }
 
-    const [isShown, setIsShown] = useState(true);
+   const [isShown, setIsShown] = useState(false);
 
     return (
       <div>
-        <textarea className="query-input" placeholder="Enter query and press Cmd/Ctrl + Enter" rows={5}></textarea>
+        <textarea className="query-input" placeholder="Enter query and press Cmd/Ctrl + Enter" rows={5} onKeyDown={keyDown} ref={myref}></textarea>
         <div style={{display: isShown ? 'flex' : 'none'}} className="query-result-header">
           <span id="msg"></span>
           <span id="result-header" className="query-result-header-buttons">
@@ -27,7 +34,6 @@ export default function MyList() {
         <span>Query History (0 queries)</span>
         <table id="results">
         </table>
-        <button onClick={handleClick}>Press me</button>
       </div>
     );
   }
